5fd93e25e74ab098686a684a7d995422a41c6495
Lars Tingelstad <lars.tingelstad@t-robotics.ai>
2024-08-04 19:25:18 +0200

    Removed automatic launch

--- install/app-kitty.sh
+++ install/app-kitty.sh
@@ -1,4 +1,4 @@
-curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin launch=n
 ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten /usr/local/bin/
 cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
 cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
