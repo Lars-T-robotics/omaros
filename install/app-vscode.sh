0dcd63f552dd994fb5e4872b7e60b8f5fd2e43d6
Lars Tingelstad <lars.tingelstad@t-robotics.ai>
2024-08-05 13:44:10 +0200

    Added vscode extensions

--- install/app-vscode.sh
+++ install/app-vscode.sh
@@ -8,4 +8,13 @@ mkdir -p ~/.config/Code/User
 cp ~/.local/share/omaros/configs/vscode.json ~/.config/Code/User/settings.json
 
 # Install default extensions
-code --install-extension vscodevim.vim catppuccin.catppuccin-vsc
+code --install-extension vscodevim.vim
+code --install-extension catppuccin.catppuccin-vsc
+code --install-extension ms-vscode.cpptools-extension-pack
+code --install-extension twxs.cmake
+code --install-extension ms-python.python
+code --install-extension ms-python.black-formatter
+code --install-extension redhat.vscode-xml
+code --install-extension redhat.vscode-yaml
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-vscode-remote.remote-containers
